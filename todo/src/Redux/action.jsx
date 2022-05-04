d9ac3342bc4d7fcace2cd67b1dcfa8aec3ce3eb5
RajarsiMukherjee <96103218+RajarsiMukherjee@users.noreply.github.com>
2022-05-04 18:35:33 +0530

    App Created

---
+++ todo/src/Redux/action.jsx
@@ -0,0 +1,14 @@
+//import { ADD_TODO, DELETE_TODO }from "./actionType"
+export const ADD_TODO = "ADD_TODO"
+export const DELETE_TODO = "DELETE_TODO"
+
+
+export const addTodo = (payload) =>  ({
+    type : ADD_TODO,
+    payload
+})
+
+export const deleteTodo = (id) => ({
+    type : DELETE_TODO,
+    id
+})
